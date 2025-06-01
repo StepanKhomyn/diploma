f183c555e21553d77b3ef710eaeddff53839bd5d
StepanKhomyn <145112517+StepanKhomyn@users.noreply.github.com>
2025-06-01 23:42:13 +0300

    Update WrongParametersException.java

--- src/main/java/com/bachelors/speecher/sox/WrongParametersException.java
+++ src/main/java/com/bachelors/speecher/sox/WrongParametersException.java
@@ -1,4 +1,4 @@
-package main.java.com.bachelors.speecher.sox;
+package com.bachelors.speecher.sox;
 
 public class WrongParametersException extends Exception {
     public WrongParametersException(String s) {
